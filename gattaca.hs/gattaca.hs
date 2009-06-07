2a63ffda8d3044165ada56e4947930828f5561a0
Jason Dew <jason.dew@gmail.com>
2009-06-07 15:50:27 -0400

    optimizing gattaca programs

--- gattaca.hs/gattaca.hs
+++ gattaca.hs/gattaca.hs
@@ -1,17 +1,18 @@
 import Data.List
+import Data.Maybe
 import System.Environment
 import System.IO
 
-data Prediction = Prediction { start :: Int, stop :: Int, score :: Int }
+data Prediction = Prediction { start :: !Int, stop :: !Int, score :: !Int }
 
 instance Show Prediction where
-  show p = " (" ++ (show $ start p) ++ "-" ++ (show $ stop p) ++ "):" ++ (show $ score p) ++ " "
+  show p = "[" ++ (show $ start p) ++ ", " ++ (show $ stop p) ++ ", " ++ (show $ score p) ++ "]"
 
 instance Eq Prediction where
   a == b = (start a == start b) && (stop a == stop b) && (score a == score b)
 
 instance Ord Prediction where
-  compare a b = start a `compare` start b
+  compare a b = stop a `compare` stop b
 
 readGattacaFormat :: Handle -> IO [Prediction]
 readGattacaFormat h = do
@@ -28,27 +29,20 @@ parsePrediction p = case map read (words p) of
                     (s : t : c : []) -> Prediction { start = s, stop = t, score = c }
 
 optimalScore :: [Prediction] -> Int
-optimalScore = const 42
-
-overlaps :: Prediction -> Prediction -> Bool
-p `overlaps` q = ((start p <= start q) && (start q <= stop p)) || ((start p <= stop q) && (stop q <= stop p))
-
-scoreSet :: [Prediction] -> Int
-scoreSet = sum . map score
+optimalScore = optimalScore' . sort where
+  optimalScore' (first:rest) = f [(stop first, score first)] (stop first) rest
+                               where f maximums _         []     = snd . head $ maximums
+                                     f maximums last_stop (p:ps) = case attempt > (snd . head $ maximums) of
+                                                                     True  -> f ((stop p, attempt) : maximums) (stop p) ps
+                                                                     False -> f maximums (stop p) ps
+                                                                   where attempt = score p + (best_less_than . start $ p)
+                                                                         best_less_than n = case find (\(point, _) -> point < n) maximums of
+                                                                                              Nothing -> 0
+                                                                                              Just q  -> snd q
 
 main :: IO ()
 main = do
   (input_filename:_) <- getArgs
   predictions        <- withFile input_filename ReadMode readGattacaFormat
-
-  putStrLn $ "n = " ++ (show . length $ predictions)
-  putStrLn $ "predictions = " ++ (show predictions)
-
-  let ss = map (\s -> ((sum $ map score s), s)) . filter nonOverlapping . subsequences $ predictions
-  let max = maximum $ map (scoreSet . snd) ss
-  let answers = filter (\s -> fst s == max) ss
-
-  putStrLn $ "max = " ++ (show max)
-  mapM_ (\answer -> putStrLn $ "answer = " ++ (show answer)) answers
 
   putStrLn . show $ optimalScore predictions
--- gattaca.rb/gattaca.rb
+++ gattaca.rb/gattaca.rb
@@ -12,7 +12,7 @@ class GattacaFormat
     lines[(prediction_count_line_number+1), prediction_count].map do |line|
       line.split(/\s+/).map {|datum| datum.to_i }
     end.sort do |a, b|
-      a[1] <=> b[1]
+      [a[1], a[0]] <=> [b[1], b[0]]
     end
   end
 
@@ -27,21 +27,13 @@ class GattacaSolver
 
   def run
     first_start, first_stop, first_score = @predictions.shift
-
     @maximums << [first_stop, first_score]
-    last_end_point = first_stop
 
     @predictions.each do |prediction|
       start, stop, score = prediction
       attempt = score + best_less_than(start)
 
-      if attempt > @maximums.first.last
-        if stop == last_end_point
-          @maximums.first.last = attempt
-        else
-          @maximums.unshift [stop, attempt]
-        end
-      end
+      @maximums.unshift [stop, attempt] if attempt > @maximums.first.last
     end
 
     @maximums.first.last
